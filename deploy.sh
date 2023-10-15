af05ad2f3e44a697b846bbf7335086e8dc17e51e
samir-ware <samir.ware@gmail.com>
2023-10-15 07:40:52 -0400

    Updated DocC documentation

---
+++ deploy.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+rm -rf docsData
+
+echo "Building DocC documentation for ModularSlothCreator..."
+
+xcodebuild  -derivedDataPath docsData -scheme SlothCreator -destination 'platform=iOS Simulator,name=iPhone 13 Pro Max'  docbuild
+
+echo "Copying DocC archives to doc_archives..."
+
+mkdir doc_archives
+
+cp -R `find docsData -type d -name "*.doccarchive"` doc_archives
+
+mkdir docs
+
+for ARCHIVE in doc_archives/*.doccarchive; do
+    cmd() {
+        echo "$ARCHIVE" | awk -F'.' '{print $1}' | awk -F'/' '{print tolower($2)}'
+    }
+    ARCHIVE_NAME="$(cmd)"
+    echo "Processing Archive: "./$ARCHIVE
+    $(xcrun --find docc) process-archive transform-for-static-hosting ./"$ARCHIVE" --hosting-base-path SlothCreator --output-path docs
+done
+
+git config user.name "$DOCC_GITHUB_NAME"
+
+git config user.email "$DOCC_GITHUB_EMAIL"
+
+# Change the GitHub URL to your repository
+echo "effective url is" ${DOCC_GITHUB_USERNAME}${DOCC_GITHUB_API_TOKEN};
+
+git remote set-url origin https://$DOCC_GITHUB_USERNAME:$DOCC_GITHUB_API_TOKEN@github.com/samir-ware/SlothCreator
+git fetch
+
+git add .
+git commit -m "Updated DocC documentation"
+git push origin main 
+
+rm -rf docs doc_archives
+
---
+++ env.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+echo "Setting environment varaibles"
+export ARCH=arm
+export DOCC_GITHUB_NAME=samir-ware
+export DOCC_GITHUB_EMAIL=samir.ware@gmail.com
+export DOCC_GITHUB_USERNAME=samir-ware
+export DOCC_GITHUB_API_TOKEN=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
+echo "Done !!"
