064052a2c8fde2f92795a254c644528cccf5cea0
samir-ware <samir.ware@gmail.com>
2023-10-15 07:55:58 -0400

    updated token

--- env.sh
+++ env.sh
@@ -4,5 +4,5 @@ export ARCH=arm
 export DOCC_GITHUB_NAME=samir-ware
 export DOCC_GITHUB_EMAIL=samir.ware@gmail.com
 export DOCC_GITHUB_USERNAME=samir-ware
-export DOCC_GITHUB_API_TOKEN=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
+export DOCC_GITHUB_API_TOKEN=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
 echo "Done !!"
